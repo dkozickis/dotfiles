bc01c179717fc63d3d319356ac480d558d421e5f
Deniss Kozickis <deniss.kozickis@gmail.com>
2021-01-14 12:08:39 +0200

    Hope this works

--- homebrew/install.sh
+++ homebrew/install.sh
@@ -1,6 +1,17 @@
 #!/bin/sh
 
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+# Check for Homebrew
+if test ! $(which brew)
+then
+  echo "  Installing Homebrew for you."
+
+  # Install the correct homebrew for each OS type
+  if [ "$(uname)" == "Darwin" ]; then
+    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+  elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/linuxbrew/go/install)"
+  fi
+fi
 
 brew install git ripgrep imagemagick watchman tree bat \
 delta switchaudio-osx exa fzf tldr jq
